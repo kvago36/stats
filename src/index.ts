aa3088021ddb74faf7ce3abc268fd8be7abba664
a.kosarchuk <a.kosarchuk@ngslab.ru>
2020-04-11 17:39:19 +0300

    add csv reader class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-import fs from 'fs'
+import { MatchReader } from './MatchReader'
+import { MatchResult } from './MatchResult'
+
+const reader = new MatchReader('original.csv')
+reader.read();
+
+let manWins = 0
+
+for (const match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manWins++
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manWins++
+  }
+}
+
+console.log(manWins)
